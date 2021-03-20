3124323859578fed196da5525b84fba7b97ddb78
Abylay <auzakbaev20@gmail.com>
2021-03-20 23:39:38 +0500

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewsComponent } from './news/news.component';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NewsComponent,
+    NewsDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: NewsComponent},
+      { path: ':newsId', component: NewsDetailComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
