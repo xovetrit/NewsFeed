ddb0c5ba2759827a4bd1e973f6cfe2089fd8bfdb
Abylay <auzakbaev20@gmail.com>
2021-03-22 04:20:35 +0500

    search news

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -9,11 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class NewsComponent implements OnInit {
 
   news = []
+  headlineSearch: any;
 
   constructor(public newsService: NewsService ) { }
 
   ngOnInit(): void {
     this.news = this.newsService.getNews()
+  }
+
+  Search(){
+    if (this.headlineSearch == '') this.ngOnInit()
+    else this.news = this.news.filter(n => n.headline.toLowerCase().match(this.headlineSearch.toLowerCase()))
   }
 
 }
